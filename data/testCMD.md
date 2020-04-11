8496ea01bc3d06f7953c5f66a2aca415a4fbd01c
todaylg <lg2lugang@gmail.com>
2020-04-11 21:49:10 +0800

    Update:BRDF LUT CMD

--- data/testCMD.md
+++ data/testCMD.md
@@ -11,3 +11,5 @@ docker container start/stop containerID
 docker exec -it containerID bash
 
 docker container prune
+
+docker run -v C:\Gallery\Code\Github\envTools/data:/data -t todaylg/envtools envBRDF -s 128 -n 1024 /data/result/brdf_ue4.bin
--- process_environment.py
+++ process_environment.py
@@ -294,7 +294,7 @@ class ProcessEnvironment(object):
         # create the integrateBRDF texture
         # we dont need to recreate it each time
         outout_filename = os.path.join(self.working_directory, "brdf_ue4.bin")
-        size = self.integrate_BRDF_size
+        size = self.integrate_BRDF_sizeintegrate_BRDF_size
         cmd = "{} -s {} -n {} {}".format(envIntegrateBRDF_cmd, size, self.brdf_nb_samples, outout_filename)
         execute_command(cmd)
 
@@ -580,10 +580,10 @@ class ProcessEnvironment(object):
         print ""
 
         # precompute lut brdf
-        start_tick = time.time()
-        self.compute_brdf_lut_ue4()
-        print "== {} compute_brdf_lut_ue4 ==".format(time.time() - start_tick)
-        print ""
+        # start_tick = time.time()
+        # self.compute_brdf_lut_ue4()
+        # print "== {} compute_brdf_lut_ue4 ==".format(time.time() - start_tick)
+        # print ""
 
         # register mipspecular
         if self.export_mipmap_cubemap:
